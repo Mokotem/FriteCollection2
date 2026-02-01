3d19478f25d029f572950d19e39bfa415a327d0b
Frite <therond.romain@gmail.com>
2026-02-01 11:12:18 +0100

    maj

---
+++ Entity/Basics.cs
@@ -0,0 +1,428 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections;
+
+namespace FriteCollection2.Entity;
+
+/// <summary>
+/// Permet de décrire une entité dans l'espace.
+/// </summary>
+public class Space : IEnumerable
+{
+    private static readonly Space spacezero = new Space(0, 0);
+    public static Space Zero => spacezero;
+
+    public static Point Camera = Point.Zero;
+
+    public static void SetDefaultEnvironment(in Environment env)
+    {
+        defaultEnvironment = env;
+    }
+    private static Environment defaultEnvironment;
+
+    private readonly Environment environment;
+
+    public class EnumCorners : IEnumerator
+    {
+        byte i;
+        Space space;
+
+        public EnumCorners(Space sp)
+        {
+            this.space = sp;
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            ++i;
+            return i < 4;
+        }
+
+        void IEnumerator.Reset()
+        {
+            i = 0;
+        }
+
+        object IEnumerator.Current =>
+            new Vector2(
+                i % 2 == 0 ? space.X : space.X + space.W,
+                i / 2 == 0 ? space.Y : space.Y + space.H);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new EnumCorners(this);
+    }
+
+
+    public Space(int width, int height)
+    {
+        Position = Vector2.Zero;
+        Scale = new Vector2(width, height);
+        this.environment = defaultEnvironment;
+    }
+
+    public Space() : this(30, 30)
+    { 
+
+    }
+
+    public Space(in Environment env) : this()
+    {
+        this.environment = env;
+    }
+
+    public Space Copy()
+    {
+        return new Space()
+        {
+            Position = Position,
+            Scale = Scale
+        };
+    }
+
+    /// <summary>
+    /// Position.
+    /// </summary>
+    public Vector2 Position;
+    public static int width, height;
+
+    public void SetPosition(Vector2 pos, Bounds centerPoint)
+    {
+        Position = pos - BoundFunc.BoundToVector(centerPoint, Scale.X, Scale.Y);
+    }
+
+    public void SetPosition(Vector2 pos, Bounds centerPoint, Bounds origin)
+    {
+        Position = pos - BoundFunc.BoundToVector(centerPoint, Scale.X, Scale.Y)
+                       + BoundFunc.BoundToPoint(origin, width, height).ToVector2();
+    }
+
+    public float GetPosX(Align origin, Align centerPos)
+    {
+        return BoundFunc.AlignToFloat(origin, width) - BoundFunc.AlignToFloat(centerPos, Scale.X);
+    }
+
+    public float GetPosY(Align origin, Align centerPos)
+    {
+        return BoundFunc.AlignToFloat(origin, height) - BoundFunc.AlignToFloat(centerPos, Scale.Y);
+    }
+
+    public Vector2 GetPos(Bounds origin, Bounds centerPos)
+    {
+        return BoundFunc.BoundToPoint(origin, width, height).ToVector2()
+             - BoundFunc.BoundToVector(centerPos, Scale.X, Scale.Y);
+    }
+
+    public Vector2 GetPos(Bounds centerPos)
+    {
+        return BoundFunc.BoundToPoint(Bounds.Center, width, height).ToVector2()
+             - BoundFunc.BoundToVector(centerPos, Scale.X, Scale.Y);
+    }
+
+    public Vector2 CenterPoint => new Vector2(CenterPointX, CenterPointY);
+    public float CenterPointX => Position.X + (Scale.X / 2f);
+    public float CenterPointY => Position.Y + (Scale.Y / 2f);
+
+    public float X
+    {
+        get => Position.X;
+        set => Position.X = value;
+    }
+    public float Y
+    {
+        get => Position.Y;
+        set => Position.Y = value;
+    }
+
+    public float W
+    {
+        get => Scale.X;
+        set => Scale.X = value;
+    }
+    public float H
+    {
+        get => Scale.Y;
+        set => Scale.Y = value;
+    }
+
+    /// <summary>
+    /// Taille.
+    /// </summary>
+    /// <remarks>Les tailles négatives sont prises en charges.</remarks>
+    public Vector2 Scale;
+
+    public override int GetHashCode()
+    {
+        return Position.GetHashCode() + (Scale.GetHashCode() * 16_777_216);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Space)
+        {
+            Space sp = (Space)obj;
+            return Scale == sp.Scale && Position == sp.Position;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Transform (position:" + Position.ToString() + ", scale:" + Scale.ToString() + ")";
+    }
+}
+
+public class SpaceDirection : Space
+{
+    public float direction = 0f;
+}
+
+public interface ILayer
+{
+    public short Layer
+    {
+        get;
+        set;
+    }
+}
+
+/// <summary>
+/// Permet de décrire l'apparence d'une entité.
+/// </summary>
+public class Renderer : ICopy<Renderer>, ILayer
+{
+    internal static Texture2D _defaultTexture, _notFoundTexture;
+    public static Texture2D DefaultTexture => _defaultTexture;
+    public static Texture2D NotFoundTexture => _notFoundTexture;
+
+    public static void SetNotFoundTexture(Texture2D tex)
+    {
+        _notFoundTexture = tex;
+    }
+
+    public static Texture2D CreateTexture(GraphicsDevice device, int w, int h, Color color)
+    {
+        Texture2D texture = new Texture2D(device, w, h);
+
+        Color[] data = new Color[w * h];
+        for (int pixel = 0; pixel < w * h; pixel++)
+        {
+            data[pixel] = color;
+        }
+
+        texture.SetData(data);
+
+        return texture;
+    }
+
+    public static Texture2D CreateNotFoundTexture(GraphicsDevice device, int w, int h)
+    {
+        Texture2D texture = new Texture2D(device, w, h);
+
+        float ws2 = w / 2 - 0.5f;
+        float hs2 = h / 2 - 0.5f;
+
+        Color[] data = new Color[w * h];
+        for (int pixel = 0; pixel < w * h; pixel++)
+        {
+            int x = pixel % w;
+            int y = pixel / w;
+            if ((x - ws2) * (y - hs2) < 0)
+            {
+                data[pixel] = new Color(255, 0, 255);
+            }
+            else
+            {
+                data[pixel] = new Color(0, 0, 0);
+            }
+        }
+
+        texture.SetData(data);
+
+        return texture;
+    }
+
+    public static void SetDefaultTexture(Texture2D t)
+    {
+        _defaultTexture = t;
+    }
+    public static void SetDefaultTexture(GraphicsDevice device, Color c)
+    {
+        _defaultTexture = CreateTexture(device, 2, 2, c);
+    }
+
+    public static void SetDefaultTexture(GraphicsDevice device)
+    {
+        SetDefaultTexture(device, Color.White);
+    }
+
+
+    public static readonly Point[] outLinePositions = new Point[8]
+    {
+        new(-1, 1),
+        new(0, 1),
+        new(1, 1),
+
+        new(-1, 0),
+        new(1, 0),
+
+        new(-1, -1),
+        new(0, -1),
+        new(1, -1)
+    };
+
+    private float layer;
+    public float GetLayer() => layer;
+
+    public SpriteEffects effect = SpriteEffects.None;
+
+    public bool outline = false;
+
+    public static Color DefaultOutline = Color.Black;
+    public Color outlineColor;
+
+    public static float ToLayer(short value)
+    {
+        if (value > 1000) throw new ArgumentOutOfRangeException("value cannot be greater than 1000");
+        if (value < -1000) throw new ArgumentOutOfRangeException("value cannot be less than -1000");
+        return (value + 1000f) / 2000f;
+    }
+
+    public static short FromLayer(float value)
+    {
+        return (short)float.Round((value * 2000) - 1000);
+    }
+
+    public short Layer
+    {
+        get => FromLayer(layer);
+        set => layer = ToLayer(value);
+    }
+
+    public static Texture2D CreateCircleTexture(GraphicsDevice device, int width)
+    {
+        Texture2D tex = new Texture2D(device, width, width);
+        Color[] data = new Color[width * width];
+        for (int i = 0; i < width; i += 1)
+        {
+            for (int j = 0; j < width; j += 1)
+            {
+                if (float.Sqrt(float.Pow(i - (width / 2), 2) + float.Pow(j - (width / 2), 2)) <= width / 2)
+                {
+                    data[i + (j * width)] = Color.White;
+                }
+                else
+                {
+                    data[i + (j * width)] = Color.Transparent;
+                }
+            }
+        }
+        tex.SetData<Color>(data);
+        return tex;
+    }
+
+    public static Texture2D CreateCircleTexture(GraphicsDevice device, int width, int holeSize)
+    {
+        Texture2D tex = new Texture2D(device, width, width);
+        Color[] data = new Color[width * width];
+        for (int i = 0; i < width; i += 1)
+        {
+            float a = float.Pow(i - (width / 2), 2);
+            for (int j = 0; j < width; j += 1)
+            {
+                float d = float.Sqrt(a + float.Pow(j - (width / 2), 2));
+                if (d <= width / 2 && d >= holeSize / 2)
+                {
+                    data[i + (j * width)] = Color.White;
+                }
+                else
+                {
+                    data[i + (j * width)] = Color.Transparent;
+                }
+            }
+        }
+        tex.SetData<Color>(data);
+        return tex;
+    }
+
+    public static Texture2D CreateFrameTexture(GraphicsDevice device, int width, int height, ushort borderSize)
+    {
+        Texture2D tex = new Texture2D(device, width, height);
+        Color[] data = new Color[width * height];
+        for (int i = 0; i < width; i += 1)
+        {
+            for (int j = 0; j < height; j += 1)
+            {
+                if (i < borderSize || j < borderSize || width - i < borderSize + 1 || height - j < borderSize + 1)
+                {
+                    data[i + (j * width)] = Color.White;
+                }
+                else
+                {
+                    data[i + (j * width)] = Color.Transparent;
+                }
+            }
+        }
+        tex.SetData<Color>(data);
+        return tex;
+    }
+
+    public Renderer()
+    {
+        Texture = _defaultTexture;
+        layer = 0.5f;
+        outlineColor = DefaultOutline;
+    }
+
+    public Renderer(short layer)
+    {
+        Texture = _defaultTexture;
+        this.layer = ToLayer(layer);
+        outlineColor = DefaultOutline;
+    }
+
+    public Renderer Copy()
+    {
+        Renderer r = new()
+        {
+            Texture = _defaultTexture,
+            Color = this.Color,
+            hide = hide
+        };
+        return r;
+    }
+
+    public Texture2D Texture { get; set; }
+
+    public Color Color = DefaultColor;
+
+    public static Color DefaultColor = Color.White;
+
+    /// <summary>
+    /// Masquer.
+    /// </summary>
+    public bool hide = false;
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Renderer)
+        {
+            Renderer re = obj as Renderer;
+            return Texture.Equals(re.Texture) && Color == re.Color
+                && hide == re.hide;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        if (Texture is null)
+        {
+            return "Renderer (texture: NULL, color:" + Color.ToString() + ")";
+        }
+        else { return "Renderer (texture: true, color:" + Color.ToString() + ")"; }
+    }
+}
---
+++ Entity/Entitys.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace FriteCollection2.Entity;
+
+/// <summary>
+/// Object.
+/// </summary>
+public class Object : Space, IDraw
+{
+    public Renderer Renderer;
+
+    public static float outlineLayer = 0.55f;
+    public float outLayer = Object.outlineLayer;
+
+    public Object() : base()
+    {
+        Renderer = new Renderer();
+    }
+
+    public Object(Texture2D texture) : this()
+    {
+        Renderer.Texture = texture;
+    }
+
+    public void DrawOutline(in SpriteBatch batch)
+    {
+        if (Renderer.outline)
+        {
+            Point pos = new Point((int)(float.Round(Position.X) - Camera.X),
+                    (int)(float.Round(Position.Y) - Camera.Y));
+
+            Point scale = new Point((int)float.Round(Scale.X),
+                    (int)float.Round(Scale.Y));
+
+            foreach (Point r in Renderer.outLinePositions)
+            {
+                batch.Draw
+                (
+                    Renderer.Texture,
+                    new Rectangle(pos + r, scale),
+                    null,
+                    Renderer.outlineColor,
+                    0,
+                    Vector2.Zero,
+                    Renderer.effect,
+                    outLayer
+                );
+            }
+        }
+    }
+
+    public void DrawBody(in SpriteBatch batch)
+    {
+        Point pos = new Point((int)(float.Round(Position.X) - Camera.X),
+                    (int)(float.Round(Position.Y) - Camera.Y));
+
+        Point scale = new Point((int)float.Round(Scale.X),
+                    (int)float.Round(Scale.Y));
+
+        batch.Draw
+        (
+            Renderer.Texture,
+            new Rectangle(pos, scale),
+            null,
+            Renderer.Color,
+            0,
+            Vector2.Zero,
+            Renderer.effect,
+            Renderer.GetLayer()
+        );
+    }
+
+    public void Draw(in SpriteBatch batch)
+    {
+        if (!Renderer.hide)
+        {
+            DrawOutline(in batch);
+            DrawBody(in batch);
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Object)
+        {
+            return base.Equals((Object)obj)
+                && Renderer.Equals(((Object)obj).Renderer);
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Object (" + base.ToString() + ", " + Renderer.ToString() + ")";
+    }
+}
+
+public class Text : IDraw
+{
+    public static Point EvaluateText(string txt, byte fw, byte fh)
+    {
+        Point result = new Point(1, 1);
+        ushort i = 0;
+        int count = 1;
+        while (i < txt.Length)
+        {
+            if (txt[i].Equals('\n'))
+            {
+                i += 2;
+                count = 1;
+                result.Y++;
+            }
+            else
+            {
+                count++;
+                if (count > result.X)
+                    result.X = count;
+                i++;
+            }
+        }
+        return new Point(result.X * fw, result.Y * fh);
+    }
+
+    private string txt;
+
+    public string Edit
+    {
+        get => txt;
+        set
+        {
+            if (txt.Length != value.Length)
+                this._scale = EvaluateText(value, 4, 6);
+            txt = value;
+        }
+    }
+
+    public float factor = 1f;
+    private Point _scale;
+    public Point Scale => _scale;
+    public Point Position;
+
+    public float Width => _scale.X * factor;
+    public float Height => _scale.Y * factor;
+
+    public Renderer Renderer;
+    public Color Background;
+
+    public void SetPosition(Point pos, Bounds b)
+    {
+        Position = pos + BoundFunc.BoundToPoint(b, Space.width, Space.height);
+        Position.X -= (int)float.Round(_scale.X * factor / 2f);
+        Position.Y -= (int)float.Round(_scale.Y * factor / 2f);
+    }
+
+    public Text(string value)
+    {
+        this.Renderer = new Renderer();
+        this._scale = EvaluateText(value, 4, 6);
+        this.txt = value;
+        Background = Color.Black;
+    }
+
+    public Text(string value, int factor)
+    {
+        this.factor = factor;
+        this.Renderer = new Renderer();
+        this._scale = EvaluateText(value, 4, 6);
+        this.txt = value;
+        Background = Color.Black;
+    }
+
+    public void Draw(in SpriteBatch batch)
+    {
+        if (!Renderer.hide)
+        {
+            if (Renderer.outline)
+            {
+                foreach (Point r in Renderer.outLinePositions)
+                {
+                    batch.DrawString(
+                        UI.Text.Font,
+                        txt,
+                        new Vector2(Position.X + 3 - Space.Camera.X + r.X, Position.Y + 1 - Space.Camera.Y + r.Y),
+                        Renderer.outlineColor,
+                        0f,
+                        Vector2.Zero,
+                        factor,
+                        SpriteEffects.None,
+                        Renderer.GetLayer() + 0.01f);
+                }
+            }
+
+            batch.DrawString(
+                FriteCollection2.UI.Text.Font,
+                txt,
+                new Vector2(Position.X + 3 - Space.Camera.X, Position.Y + 1 - Space.Camera.Y),
+                Renderer.Color,
+                0f,
+                Vector2.Zero,
+                factor,
+                SpriteEffects.None,
+                Renderer.GetLayer());
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Text " + txt + " (" + Renderer.ToString() + ")";
+    }
+}
